cd4056c67d90ae4743332bff4237bfbdc9231606
Mateusz <mateusz.wolnica@gmail.com>
2025-02-05 14:17:54 +0100

    .

--- Keycloak.NET.Client.FluentAPI/Keycloak.NET.Client.FluentAPI/Model/AccessTokenResponse.cs
+++ Keycloak.NET.Client.FluentAPI/Keycloak.NET.Client.FluentAPI/Model/AccessTokenResponse.cs
@@ -1,39 +1,39 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace Keycloak.NET.FluentAPI.Model
 {
     public class AccessTokenResponse
     {
 
-        [JsonProperty("access_token")]
+        [JsonPropertyName("access_token")]
         public String token;
 
-        [JsonProperty("expires_in")]
+        [JsonPropertyName("expires_in")]
         public long expiresIn;
 
-        [JsonProperty("refresh_expires_in")]
+        [JsonPropertyName("refresh_expires_in")]
         public long refreshExpiresIn;
 
-        [JsonProperty("refresh_token")]
+        [JsonPropertyName("refresh_token")]
         public String refreshToken;
 
-        [JsonProperty("token_type")]
+        [JsonPropertyName("token_type")]
         public String tokenType;
 
-        [JsonProperty("id_token")]
+        [JsonPropertyName("id_token")]
         public String idToken;
 
-        [JsonProperty("not-before-policy")]
+        [JsonPropertyName("not-before-policy")]
         public int notBeforePolicy;
 
-        [JsonProperty("session_state")]
+        [JsonPropertyName("session_state")]
         public String sessionState;
 
         public Dictionary<String, Object> otherClaims = new Dictionary<String, Object>();
 
-        [JsonProperty("scope")]
+        [JsonPropertyName("scope")]
         public String scope;
     }
 }
